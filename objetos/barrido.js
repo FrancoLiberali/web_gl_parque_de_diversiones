dd0d2bd740fef374f012ea5fea42f6798699bafe
Matias <matiaseiglesias@yahoo.com>
2019-10-05 23:14:40 -0300

    Se reemplazan los nombres de las variables dentro del algoritmo de barrido por nombres mas representativos

--- objetos/barrido.js
+++ objetos/barrido.js
@@ -12,25 +12,28 @@ function barrido(curva, curvaDerivada, pasos, superficieInicial) {
   for (var i = 0; i <= 1; i += pasos) {
     var binormal = vec3.create();
     var normal = vec3.create();
-    var ctg = curvaDerivada(i);
-    vec3.normalize(ctg, ctg);
-    var binormal = vec3.cross(binormal, ctg, vec3.fromValues(0, 0, 1));
-    var normal = vec3.cross(normal, binormal, ctg);
+    var tangente = curvaDerivada(i);
+    vec3.normalize(tangente, tangente);
+    var binormal = vec3.cross(binormal, tangente, vec3.fromValues(0, 0, 1));
+    var normal = vec3.cross(normal, binormal, tangente);
 
     var m1 = mat4.fromValues(binormal[0], binormal[1], binormal[2], 0,
-      ctg[0], ctg[1], ctg[2], 0,
+      tangente[0], tangente[1], tangente[2], 0,
       normal[0], normal[1], normal[2], 0,
       curva(i)[0], curva(i)[1], curva(i)[2], 1
     );
 
+    //Se aplica la tranformacion correspondiente a cada punto de la superficie
+    //actualizando a la misma  
     for (var punto = 0; punto < superficie.length; punto++) {
 
-      var formaEnNivel = vec3.create();
-      vec3.transformMat4(formaEnNivel, superficie[punto], m1);
-
-      superficie[punto] = formaEnNivel;
-
-      vertexArray = llenarNivel(vertexArray, formaEnNivel);
+      var nuevoPuntoTransformado = vec3.create();
+      //Aplico transformacion
+      vec3.transformMat4(nuevoPuntoTransformado, superficie[punto], m1);
+      //Actualizo la superficie
+      superficie[punto] = nuevoPuntoTransformado;
+      //Agrego el punto a la arreglo de vertices
+      vertexArray = llenarNivel(vertexArray, nuevoPuntoTransformado);
 
     }
   }
